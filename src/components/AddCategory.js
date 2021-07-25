d2f1958286df005391bdaff3710a0f370581ee04
iOznny <f000170014@gmail.com>
2021-07-24 23:41:40 -0500

    Agregar elemento al arreglo

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const AddCategory = ({ setCategories }) => {
 
     // Inicio del input
-    const [inputValue, setInputValue] = useState();
+    const [inputValue, setInputValue] = useState('');
 
     // Cambiar valor del input
     const handleInputChange = (e) => {        

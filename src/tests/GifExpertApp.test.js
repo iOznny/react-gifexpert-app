731c2c7e87ed20fd1c759717062e9237c5dc719d
iOznny <f000170014@gmail.com>
2021-09-16 17:39:13 -0500

    Change

--- src/tests/GifExpertApp.test.js
+++ src/tests/GifExpertApp.test.js
@@ -11,6 +11,7 @@ describe('Pruebas en GifExpertApp', () => {
     test('Show list of categories', () => {
         const categories = ['One Punch', 'Space X'];
         const wrapper = shallow(<GifExpertApp defaultCategories={ categories } />);
+        
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('GifGrid').length).toBe(categories.length);
     });    

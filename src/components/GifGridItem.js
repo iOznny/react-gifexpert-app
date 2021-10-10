b4fc1150e9aa8b42d4506391d7c2a7538f97a30b
iOznny <f000170014@gmail.com>
2021-10-10 17:56:17 -0500

    c

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const GifGridItem = ({ title, url }) => {    
-
     return (
         <div className="card animate__animated animate__bounce">
             <img src={ url } alt={ title } />
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const useFetchGifs = (category) => {
-    const [state, setState] = useState({
+    const [ state, setState ] = useState({
         data: [],
         loading: true
     });
